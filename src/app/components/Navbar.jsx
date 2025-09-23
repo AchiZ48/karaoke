27f3eb9f84bee8a4ba81d4f787be52fd01d8bb58
AchiZ48 <cemefe11@hotmail.com>
2025-09-24 03:42:52 +0700

    update svg

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -45,7 +45,10 @@ export default function Navbar() {
         { href: "/promotions", label: "Promotions" },
         { href: "/booking", label: "Booking" },
         { href: "/admin", label: "Admin Panel" },
-        { href: "/profile", label: "Profile" },
+        {
+          href: "/profile",
+          icon: <img src="/profile.svg" alt="" width={20} />,
+        },
         {
           action: () => signOut(),
           label: "Log out",
