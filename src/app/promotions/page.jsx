b3ca819657bfe66237745155aaf15e323c160ece
OPPAIxDRAGON <sivatpat@hotmail.com>
2025-09-23 21:54:09 +0700

    Update page.jsx

--- src/app/promotions/page.jsx
+++ src/app/promotions/page.jsx
@@ -23,7 +23,7 @@ export default async function PromotionsPage() {
           {promos.map((p) => (
             <div
               key={p._id}
-              className="rounded-2xl shadow-lg px-8 py-6 bg-gradient-to-br from-[#6C63FF] to-[#2B174D] text-white"
+              className="rounded-2xl shadow-lg px-8 py-6 bg-gradient-to-br from-[#7b7bbd] to-[#2d184a] text-white"
             >
               <div className="text-lg font-bold mb-2">{p.name} !</div>
               <div className="text-sm mb-4 flex items-center gap-2 flex-wrap">
