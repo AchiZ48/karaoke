03b7f222189f1cc3e603e368b8fdb8e7dbd632e6
AchiZ48 <cemefe11@hotmail.com>
2025-09-24 04:40:28 +0700

    Toast noti position

--- src/app/components/toast/ToastProvider.jsx
+++ src/app/components/toast/ToastProvider.jsx
@@ -28,7 +28,7 @@ export function ToastProvider({ children }) {
   return (
     <ToastContext.Provider value={value}>
       {children}
-      <div className="fixed top-4 right-4 z-50 space-y-2">
+      <div className="fixed bottom-4 right-4 z-50 space-y-2">
         {toasts.map((t) => (
           <div
             key={t.id}
