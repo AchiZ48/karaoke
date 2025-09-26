93c657275969e22eab7988ff5fca4238c9e9d9c0
AchiZ48 <cemefe11@hotmail.com>
2025-09-27 04:12:09 +0700

    add time slot to availability

--- src/app/my-bookings/ActionsCell.jsx
+++ src/app/my-bookings/ActionsCell.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useToast } from "../components/toast/ToastProvider";
 
 const timeSlots = [
+  "10:00-12:00",
   "12:00-14:00",
   "14:00-16:00",
   "16:00-18:00",
